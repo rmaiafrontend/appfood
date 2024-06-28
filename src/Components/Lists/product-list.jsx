44ef02f7d994b144bc91fc00f6ca35abc236e9ff
rmaiafrontend <rmaiafrontend@gmail.com>
2024-06-28 10:01:47 -0300

    Adicionando imagens dos produtos

--- src/Components/Lists/product-list.jsx
+++ src/Components/Lists/product-list.jsx
@@ -8,7 +8,7 @@ const ProductList = () => {
   return (
     <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {produtosComDesconto.map((produto) => (
-        <ProductItem key={produto.id} id={produto.id} nome={produto.nome_do_produto} preco={produto.preço} desconto={produto.desconto} imagem={produto.imagem} />
+        <ProductItem key={produto.id} id={produto.id} nome={produto.nome_do_produto} preco={produto.preço} desconto={produto.desconto} image={produto.imagem} />
       ))}
     </div>
   );
